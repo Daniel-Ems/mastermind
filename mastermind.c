c80407e1dc87f03b9414ad5f23f3dcd70acf4e24
Daniel Ems <danielems3@outlook.com>
2016-10-28 08:04:04 -0400

    Start to print numbers

--- mastermind.c
+++ mastermind.c
@@ -4,14 +4,17 @@
 
 int main(void)
 {
-char number[16];
-size_t one_digit = strlen(number);
-int first_digit = number[one_digit - 3];
-//int my_num = 1234;
 printf("Give me a number hombre");
-fgets(number, sizeof(number), stdin); 
-printf("%s\n", number);
-printf("%d", first_digit); 
+int numbers[] = {1234};
+
+ 
+
+
+//int my_num = 1234;
+
+ 
+printf("%d\n", *numbers);
+
 
 
 }
