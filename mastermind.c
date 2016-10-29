46ab1cc2b30aaac15aa29244f305b7bf187a4a38
Daniel Ems <danielems3@outlook.com>
2016-10-28 22:47:35 -0400

    clean code

--- mastermind.c
+++ mastermind.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <strings.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
@@ -10,28 +9,14 @@ void number_strip(char array[]);
 int main(void)
 {
 
-
-//random number generated, 
 time_t t;
 srand((unsigned) time(&t));
-
-//user input for random number
 
 char users[10];
 char numbers[5] ;
 rand_function(numbers);
 
 
-//number_strip(users);
-   
-//printf("%s\n:users", users);
-
-
-	int a = 1;
-	int b = 2;
-
-
-//number_strip(numbers);
 
 while(users != numbers){
 
@@ -44,6 +29,8 @@ while(users != numbers){
 	printf("%s\n", numbers); //<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
+	int a = 1;
+	int b = 2;
 
 	char red_counts[5] = {'0','0','0','0','\0'} ;
 	for(a=0; a<4; a++){
@@ -61,22 +48,6 @@ while(users != numbers){
 		}
 	} 
 
-/*
-	
-	if((users[c] == numbers[a] || users[c] == numbers[b] || users [c] == numbers[d]) && (users	[c] != numbers[c])){	
-		white_counter += 1;
-	}
-	if((users[b] == numbers[a] || users[b] == numbers[c] || users [b] == numbers [d]) && (users	[b] != numbers[b])){	
-		white_counter += 1;	
-	}
-	if((users[a] == numbers[c] || users[a] == numbers[b] || users [a] == numbers[d]) && (users	[a] != numbers[a])){
-		white_counter += 1;	
-	}
-	if((users[d] == numbers[a] || users[d] == numbers[b] || users [d] == numbers [c]) && 		(users	[d] != numbers[d])){
-		white_counter += 1;	
-
-	}
-*/
 	if(red_counter == 4){
 		break;
 	}
@@ -86,16 +57,6 @@ while(users != numbers){
 	}
 printf("You won\n");
 }
-
-void number_strip(char array[])
-{
-	int a = 0;
-	for(a=0; a < 4; a++){
-		array[a] = array[a]-48;
-	}
-	return;
-}
-
 
 void rand_function(char array[])
 {
