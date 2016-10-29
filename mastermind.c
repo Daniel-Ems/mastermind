634c09e177c4bf495bd18b125ba12a3a084fda38
Daniel Ems <danielems3@outlook.com>
2016-10-29 07:41:22 -0400

    Create an additional dummy array in attempt to use as temporary place holders for string values

--- mastermind.c
+++ mastermind.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-
+#include <string.h>
 
 void rand_function( char array []);
 void number_strip(char array[]);
@@ -13,6 +13,7 @@ time_t t;
 srand((unsigned) time(&t));
 
 char users[10];
+char fake_users[10];
 char numbers[5] ;
 rand_function(numbers);
 int counter = 0;
@@ -23,35 +24,42 @@ while(users != numbers){
 	int red_counter = 0;
 	int white_counter =0;
 	
+	
+	char fake_numbers[5];
+	
+
+	strncpy(fake_numbers,numbers,5);
 
 	printf("\nPlease give me a number: "); //<<<<<<<<<<<<<<<<<
 	fgets(users, sizeof(users), stdin);
 	counter += 1;
-
+	strncpy(fake_users,users,5);
 	printf("%s\n", numbers); //<<<<<<<<<<<<<<<<<<<<<<<<<
 
-
+	printf("%s fake_users\n", fake_users);
+	printf("%s fake_numbers\n", fake_numbers);
 	int a = 1;
 	int b = 2;
 
 	char red_counts[5] = {'0','0','0','0','\0'} ;
 	char white_counts[5] = {'0','0','0','0','\0'};
 	for(a=0; a<4; a++){
-		if(users[a] == numbers[a]){
+		if(fake_users[a] == fake_numbers[a]){
 			red_counter += 1;
-			red_counts[a] = users[a];
+			fake_users[a] = 'R';
 		}
 	}
 
 	for(a=0; a<4; a++){
 		for(b=0; b<4; b++){
-			if(users[a] == numbers[b] && users[a] != red_counts[b]){
-				white_counts[a] = numbers[b]; 
+			if(fake_users[a] == fake_numbers[b]){
+				white_counter +=1;					
+				fake_numbers[b] = 'W'; 
+				fake_users[a] = 'x';
 				
 			}
 		}
 	} 
-
 	if(red_counter == 4){
 		break;
 	}
